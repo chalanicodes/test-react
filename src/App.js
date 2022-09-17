c75f313ebb511913de247a870af6d54d9228dec0
Chalani Dissanayaka <chalani@Chalanis-MacBook-Air.local>
2022-09-17 22:52:03 +1000

    updated App-header with yellow color

--- src/App.js
+++ src/App.js
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chalani <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My New App</p>
+        
       </header>
     </div>
   );
